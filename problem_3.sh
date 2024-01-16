a312d0437670c3ea9092e40b24f56f4b25a4b7ca
pevic sanch <sanchez.pv@pm.me>
2024-01-16 09:39:37 +0100

    adding problem 3 with solution

---
+++ problem_3.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+## problem 3
+## Write a Bash script which accepts as input and displays the greeting "Welcome (name)"
+
+# Variables
+read -p "Enter your name: " name
+
+#show the name
+
+echo "Welcome $name"
